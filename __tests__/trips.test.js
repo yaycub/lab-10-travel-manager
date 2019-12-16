e43ea5c05f1d3f099d43448d0044d76d17f4ccae
Jacob Harrington <jharrington4@gmail.com>
2019-12-16 14:50:20 -0800

    trips get all and post routes finished and tests passing

---
+++ __tests__/trips.test.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Trip = require('../lib/models/Trip');
+
+describe('app routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  let trip;
+  beforeEach(async() => {
+    trip = await Trip.create({
+      location: 'Portland',
+      startDate: new Date('2019-12-21'),
+      endDate: new Date('2019-12-29')
+    });
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can create a trip', () => {
+    return request(app)
+      .post('/api/v1/trips')
+      .send({
+        location: 'Portland',
+        startDate: Date.now(),
+        endDate: Date.now()
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          location: 'Portland',
+          startDate: expect.any(String),
+          endDate: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+
+  it('can get all trips', async() => {
+    const trips = await Trip.create([
+      { location: 'Portland', startDate: Date.now(), endDate: Date.now() },
+      { location: 'Chicago', startDate: Date.now(), endDate: Date.now() },
+      { location: 'Munich', startDate: Date.now(), endDate: Date.now() }
+    ]);
+
+    return request(app)
+      .get('/api/v1/trips')
+      .then(res => {
+        trips.forEach(trip => {
+          expect(res.body).toContainEqual({
+            _id: trip._id.toString(),
+            location: trip.location
+          });
+        });
+      });
+  });
+});
